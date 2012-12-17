9bc8a8db711c00c92cf93318a5a133c33dff0bab
David Parrish <daveparrish@gmail.com>
2012-12-17 10:17:39 -0500

    Initial commit.  Includes a complete, working definition file for installing Owl2 to a point where the postinstall script will work.

---
+++ definitions/Owl-2.0-release-i386/definition.rb
@@ -0,0 +1,44 @@
+Veewee::Definition.declare({
+  :cpu_count => '1', :memory_size=> '256',
+  :disk_size => '10140', :disk_format => 'VDI', :hostiocache => 'off',
+  :use_sata => false,
+  :use_pae => 'on',
+  :os_type_id => 'Linux26',
+  :iso_file => "Owl-2.0-release-i386.iso", :iso_src => "",
+  :iso_md5 => "2c261739f8c28f0b8ce59604e2d44041", :iso_download_timeout => 1000,
+  :iso_download_instructions => "The Owl 2 iso is gzipped at several mirrors linked from Owl's homepage.  On such mirror location is: ftp://mirrors.kernel.org/openwall/Owl/2.0-release/iso/Owl-2.0-release-i386.iso.gz",
+  :boot_wait => "10",
+  :boot_cmd_sequence => [ 
+    '<Down><Down><Enter>',			# Start boot
+    '<Wait>' * 20,
+    'settle<Enter>',				# Start installation wizard
+    '<Enter><Enter>',				# Enter fdisk
+    'n<Enter>p<Enter>1<Enter><Enter>+256M<Enter>t<Enter>82<Enter>',	# Partition harddrive swap space
+    'n<Enter>p<Enter>2<Enter><Enter><Enter>',	# Create root partition
+    'w<Enter>',					# Write table
+    '<Wait>' * 10 + 'q',			# Wait for write
+    '<Enter><Enter><Enter><Left><Enter>' + '<Wait>' * 10,	# Format and mount root partition 
+    't<Enter>q',				# Setup tmpfs
+    's<Enter><Enter><Left><Enter>q',		# Activate Swap
+    'i' + '<Wait>' * 120,			# Install packages (Wait 2 minutes for install)
+    'k' + '<Down>' * 123 + '<Wait><Enter><Enter><Left><Enter><Enter>',	# Setup keyboard
+    'p' + 'Vagrant1234!<Enter>' * 2,		# Configure temporary root passwd
+    't<Enter>q',				# Configure /etc/fstab
+    'z<Down><Enter>' + '<Down>' * 84 + '<Wait><Enter><Enter>',	# Configure /etc/fstab
+    'nh' + '<Delete>' * 9 + 'vagrant.com<Enter>',	# Set hostname
+    'ia<Enter>10.0.2.15<Enter>/24<Enter>q',	# Add eth0 interface
+    'g<Delete>2<Enter>',			# Set gateway
+    'na10.0.2.3<Enter>q',			# Set nameserver
+    's<Enter>',					# Save network settings
+    'b/dev/hda<Enter>',				# kernel and bootloader
+    'r<Left><Enter>', # + '<Wait>' * 30,		# Reboot
+    #'root<Enter>Vagrant1234!<Enter>',		# Login
+  ],
+  :virtualbox => { :vm_options => [{:natdnsproxy1 => 'on'}] },
+  :kickstart_port => "7122", :kickstart_timeout => 10000, :kickstart_file => "",
+  :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "Vagrant1234!", :ssh_key => "",
+  :ssh_host_port => "7222", :ssh_guest_port => "22",
+  :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
+  :shutdown_cmd => "poweroff",
+  :postinstall_files => [ "postinstall.sh"], :postinstall_timeout => 10000
+})
---
+++ definitions/Owl-2.0-release-i386/postinstall.sh
@@ -0,0 +1,4 @@
+#http://chrisadams.me.uk/2010/05/10/setting-up-a-centos-base-box-for-development-and-testing-with-vagrant/
+#kernel source is needed for vbox additions
+
+echo "We made it to the post install!"
