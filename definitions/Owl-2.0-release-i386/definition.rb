b22666a048d77a296691b9349f8104fa9ae466e0
David Parrish <daveparrish@gmail.com>
2012-12-18 00:17:17 -0500

    Changes root password to "vagrant" Changes host to vagrant-owl2.com Tries to get the right source files installed so that guest additions can be installed.

--- definitions/Owl-2.0-release-i386/definition.rb
+++ definitions/Owl-2.0-release-i386/definition.rb
@@ -22,22 +22,29 @@ Veewee::Definition.declare({
     's<Enter><Enter><Left><Enter>q',		# Activate Swap
     'i' + '<Wait>' * 120,			# Install packages (Wait 2 minutes for install)
     'k' + '<Down>' * 123 + '<Wait><Enter><Enter><Left><Enter><Enter>',	# Setup keyboard
-    'p' + 'Vagrant1234!<Enter>' * 2,		# Configure temporary root passwd
+    "!sed -i 's/enforce=everyone/enforce=none/g' /owl/etc/pam.d/system-auth<Enter>",	# Turn off strong password enforement
+    'exit<Enter>',
+    'p' + 'vagrant<Enter>' * 2,		# Configure temporary root passwd
     't<Enter>q',				# Configure /etc/fstab
     'z<Down><Enter>' + '<Down>' * 84 + '<Wait><Enter><Enter>',	# Configure /etc/fstab
-    'nh' + '<Delete>' * 9 + 'vagrant.com<Enter>',	# Set hostname
+    'nh' + '<Delete>' * 9 + 'vagrant-owl2.com<Enter>',	# Set hostname
     'ia<Enter>10.0.2.15<Enter>/24<Enter>q',	# Add eth0 interface
     'g<Delete>2<Enter>',			# Set gateway
     'na10.0.2.3<Enter>q',			# Set nameserver
     's<Enter>',					# Save network settings
-    'h' + '<Wait>' * 10,			# Install kernel headers
+    '!tar -jxvf /usr/src/kernel/linux-2.4.32.tar.bz2 -C /owl/usr/src<Enter>',	# Copy kernel source for building Guest Additions
+    '<Wait>' * 10,
+    'mv /owl/usr/src/linux-2.4.32 /owl/usr/src/linux<Enter>',
+    'rm -rf /owl/usr/src/linux/include<Enter>',
+    'exit<Enter>',
+    'h' + '<Wait>' * 10 + '<Enter>',			# Install kernel headers
     'b/dev/hda<Enter>',				# kernel and bootloader
     'r<Left><Enter>', # + '<Wait>' * 30,		# Reboot
-    #'root<Enter>Vagrant1234!<Enter>',		# Login
+    #'root<Enter>vagrant<Enter>',		# Login
   ],
   :virtualbox => { :vm_options => [{:natdnsproxy1 => 'on'}] },
   :kickstart_port => "7122", :kickstart_timeout => 10000, :kickstart_file => "",
-  :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "Vagrant1234!", :ssh_key => "",
+  :ssh_login_timeout => "10000", :ssh_user => "root", :ssh_password => "vagrant", :ssh_key => "",
   :ssh_host_port => "7222", :ssh_guest_port => "22",
   :sudo_cmd => "echo '%p'|sudo -S sh '%f'",
   :shutdown_cmd => "poweroff",
